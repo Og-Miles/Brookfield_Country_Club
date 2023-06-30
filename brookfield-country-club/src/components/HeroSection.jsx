eaff892b0f0d676ea4214251faf6227ec0424910
Emmanuel Ogbuji <emmanuelogbuji5@gmail.com>
2023-06-30 23:26:47 +0100

    Hero section

--- brookfield-country-club/src/components/HeroSection.jsx
+++ brookfield-country-club/src/components/HeroSection.jsx
@@ -1,13 +1,12 @@
 
 import { avatar1,avatar2,avatar3,Beachpoint1,Beachpoint2,Beachpoint3,count1,count2,count3,Discount,gradient1,gradient2,gradient3,gradient4,gradient5, Hero,Logo,special,Luxury1,Luxury2,sponsor1,sponsor2,video,dotted,umbrella,Frame  } from "../assets/index";
 
-import Date from '../containers/Date'
 import styles from "../style";
 
 const HeroSection = () => (
     <div className={`w-full bg-white px-16 py-6 ${styles.flexCenter}`}>
         
-     <div className="flex flex-row justify-between">
+     <div className={`flex flex-row justify-between ${styles.flexCenter}`}>
       <div>
      <form action="#" className="flex flex-col mr-5">
       <label htmlFor="Arrival" className={`font-Roboto font-normal text-[14px] tracking-widest mb-1`}>ARRIVAL</label>
@@ -37,7 +36,7 @@ const HeroSection = () => (
 
 
      
-        <input type="submit" value={'Check Availability'} className="bg-active px-[30px] text-white font-Roboto font-normal h-100%"/>
+        <button className={`btn mt-6`}>Check Availability</button>
      </div>
          
     </div>
