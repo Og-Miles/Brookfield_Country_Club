559f4e169ba1b44961e237d4cd751f59614cb1db
Emmanuel Ogbuji <emmanuelogbuji5@gmail.com>
2023-07-14 14:43:21 +0100

    Resort completed

--- brookfield-country-club/src/App.jsx
+++ brookfield-country-club/src/App.jsx
@@ -39,6 +39,7 @@ const App = () => (
       <div>
          <RedDivider/>
       </div>
+
        <Resort/>
     </div>
 
--- brookfield-country-club/src/components/Resort.jsx
+++ brookfield-country-club/src/components/Resort.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import styles from '../style'
+import { Luxury1, Luxury2 } from '../assets'
 
 const Resort = () => {
   return (
-    <div>Resort</div>
+    <div className={` ${styles.marginY}`}>
+        <div className={`flex flex-col flex-1 lg:flex-row items-center justify-between ${styles.marginX}`}>
+            <div>
+              <img src={Luxury1} alt="Luxury" />
+            </div>
+
+            <div className='flex-col xs:flex-start'>
+              <h1 className={`${styles.heading1}`}>Discover a New <br /> Look of Luxury Hotel</h1>
+              <p className={`${styles.paragraph} mb-3`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Cupiditate perferendis quia libero tenetur saepe repellat! <br /> Dolore repudiandae similique quae excepturi.</p>
+              <button>Read More</button>
+            </div>
+        </div>
+
+        {/* Section 1B */}
+        <div className={`flex flex-col flex-1 lg:flex-row-reverse items-center justify-between ${styles.marginX} ${styles.marginY}`}>
+            <div>
+              <img src={Luxury2} alt="Luxury" />
+            </div>
+
+            <div className='flex-col xs:flex-start'>
+              <h1 className={`${styles.heading1}`}>Enjoy the Fanciest <br />  Yacht Resort</h1>
+              <p className={`${styles.paragraph} mb-3`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Cupiditate perferendis quia libero tenetur saepe repellat! <br /> Dolore repudiandae similique quae excepturi.</p>
+              <button>Read More</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
