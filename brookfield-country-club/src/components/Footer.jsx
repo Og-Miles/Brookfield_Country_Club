8c24ca63ad21684eb6c292bbf403470f924d802e
Emmanuel Ogbuji <emmanuelogbuji5@gmail.com>
2023-07-17 16:13:06 +0100

    Final

--- brookfield-country-club/src/components/Footer.jsx
+++ brookfield-country-club/src/components/Footer.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Logo, sponsor1, sponsor2 } from "../assets"
+import styles from "../style"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="bg-[#0C112B]">
+
+    <div className={`${styles.marginX}`}>
+      
+        <div className="justify-between flex flex-col md:flex-row pb-14">
+          <div>
+            <img src={Logo} alt="Logo" className="w-[128px] h-[64px] mt-16"/>
+          </div>
+
+          <div className="flex">
+            <img src={sponsor1} alt="sponsor1" className="w-[150px] h-[150px] mt-16 mr-7"/>
+            <img src={sponsor2} alt="sponsor2" className="w-[150px] h-[150px] mt-16"/>
+          </div>
+          
+        </div>
+
+        <div className="border-t-2 border-[#66676b2f]">
+          <div className={`flex flex-col md:flex-row justify-between mt-2 ${styles.marginX}`}>
+            <p className={`${styles.paragraph} mb-5`}>© 2023 Beachpoint Resort - All Rights Reserved</p>
+            <p className={`${styles.paragraph}`}>Terms Of Service | Privacy Policy</p>
+          </div>
+        </div>
+
+    </div>
+    </div>
   )
 }
 
